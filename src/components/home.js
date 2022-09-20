8d9073032378914cba03e36507ce94e01dcaf6c6
thebabellibrarybot <jack@thebabel.org>
2022-09-20 11:25:30 -0400

    icon

--- src/components/home.js
+++ src/components/home.js
@@ -25,3 +25,34 @@ const Home = () => {
 
 
 export default Home
+
+
+
+/* bib 
+
+load animations: 
+https://stackoverflow.com/questions/51939788/how-to-trigger-react-transitiongroup-and-transition-to-animate-onload
+https://medium.com/@davidfalconbernhard/how-to-animate-once-on-page-load-in-react-js-e357fd899a4b
+
+http://reactcommunity.org/react-transition-group/transition
+
+change favicon: 
+https://noaheakin.medium.com/changing-the-default-react-browser-tab-title-and-icon-1240239d92d3
+https://www.thewindowsclub.com/how-to-make-an-icon-for-windows-10
+https://www.xiconeditor.com/
+
+
+Port inspo: 
+https://www.youtube.com/watch?v=bmpI252DmiI
+
+
+Hide a Div:
+https://www.w3docs.com/snippets/html/how-to-create-a-hidden-div-without-a-line-break-or-horizontal-space.html#:~:text=Add%20a%20%3Cdiv%3E%20element%20with%20the%20id%20attribute,the%20display%20property%20to%20%E2%80%9Cnone%E2%80%9D%20for%20the%20%E2%80%9Chide%E2%80%9D.
+
+
+
+
+
+
+
+*/ 
